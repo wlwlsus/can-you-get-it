a2b713f0bba2b6f76b08cd4338ac2272784a4a09
swj <159632_@naver.com>
2023-08-25 20:27:44 +0900

    Test: deductUserCash 테스트 코드 작성

--- cygi-user/src/test/java/com/allback/cygiuser/service/UserServiceImplTest.java
+++ cygi-user/src/test/java/com/allback/cygiuser/service/UserServiceImplTest.java
@@ -71,4 +71,41 @@ class UserServiceImplTest {
 
 		verify(passbookRepository).save(any(Passbook.class)); // save() 호출 확인
 	}
+
+	@Test
+	void deductUserCash() {
+		// given: 사용자와 관련된 객체들을 생성하고 설정
+		long userId = 1L;
+		int cash = 1000;
+		int deductCash = 500;
+		Passbook passbook = Passbook.builder().cash(cash).build();
+		Users user = Users.builder().userId(userId).passbookId(passbook).build();
+
+		when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+		when(passbookRepository.findById(user.getPassbookId().getPassbookId())).thenReturn(Optional.of(passbook));
+
+		try {
+			userService.deductUserCash(userId, deductCash);
+
+			// then: passbok의 cash가 올바르게 차감되었는지 확인
+			assertEquals(500, passbook.getCash());
+
+			verify(userRepository).save(eq(user)); // User 객체가 저장되었는지 확인
+
+			verifyNoMoreInteractions(passbookRepository); // 다른 메소드 호출이 없는지 확인
+
+		} catch (BaseException e) {
+			fail("예외 발생하지 않아야 한다.");
+		}
+
+		try {
+			int invalidPrice = 1500; // 잔액보다 큰 금액으로 호출할 경우 예외 발생
+
+			userService.deductUserCash(userId, invalidPrice);
+
+			fail("잔액 부족 예외가 발생해야 한다.");
+		} catch (BaseException e) {
+			assertEquals(ErrorMessage.NOT_ENOUGH_CASH.getErrorMessage(), e.getErrorMessage().get(0));
+		}
+	}
 }
